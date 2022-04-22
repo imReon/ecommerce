78a8eb096db0597c980a8fa7b04742bbdc2220be
Your Name <you@example.com>
2022-04-22 16:01:24 +0530

    complete server connect and database connection

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const Sequelize = require('sequelize');
+const dbConfig = require('./config/config.json');
+const serverConfig = require('./config/server.config')
+
+const env = "development";
+const dbSettings = dbConfig[env];
+const sequelize = new Sequelize(
+  dbSettings.database,
+  dbSettings.username,
+  dbSettings.password,
+  dbSettings.dialectInformation
+);
+
+
+app.listen(serverConfig.PORT, async ()=>{
+  console.log("server is running on port 3000");
+  try{
+    await sequelize.authenticate();
+    console.log("connected to db")
+  }
+  catch(error){
+    console.log("unable to connect to db: ", error)
+  }
+})
